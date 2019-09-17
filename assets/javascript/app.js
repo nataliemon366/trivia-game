49b182379a8c9a415bd37a79cb3dae7782993e92
Natalie Montano <nat.montano366@gmail.com>
2019-09-17 01:36:32 -0700

    added questions and layout for timers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,127 +1,140 @@
-$('#start').on('click', function () {
-    $('#start').remove();
-    game.loadQuestion();
-})
-$(document).on('click', '.answer-button', function (e) {
-    game.clicked(e);
-})
+// initial values
+let counter = 10;
+let currentQuestion = 0;
+let score = 0;
+let lost = 0;
+let timer;
+//if timer is over go 2 next question
 
-var questions = [{
-    question: "1. What dates does the purge fall on in 'The Purge'?",
-    answer: ["December 25- December 26", "Dec. 31st- Jan 1", "March 21 - March 22", "May 13 - May 14"],
-    correctAnswer: "March 21 - March 22",
-    image: "assets/images/"
-}, {
-    question: "2. What is the name of the Villain in 'The Ring'?",
-    answer: ["camille", "Samara", "Audrey", "Naomi "],
-    correctAnswer: "Samara",
-    image: "assets/images/"
-}, {
-    question: "3. Which movie was based on a video game?",
-    answer: ["Texas Chainsaw Massacre", "Silent Hill", "Child's Play", "Halloween "],
-    correctAnswer: "Silent Hill",
-    image: "assets/images/"
-
-}, {
-    question: "4. In 'Saw', where was Amanda's key hidden?",
-    answer: ["In the intestines of the guy next to her", "In a scorching hot oven", "Behind her eye", "In a safe covered in spikes"],
-    correctAnswer: "5. In the intestines of the guy next to her",
-    image: "assets/images/"
-
-}, {
-    question: "6. Which horror movie came out in 1979?",
-    answer: ["Alien", "Black Christmas", "A Nightmare on Elm Street", "Poltergeist"],
-    correctAnswer: "Alien",
-    image: "assets/images/"
-
-}, {
-    question: "7. Which horror film does the quote 'The Power of Christ compels you!' come from?",
-    answer: ["An American Haunting", "Halloween", "A Nightmare on Elm Street", "The Exorcist"],
-    correctAnswer: "The Exorcist",
-    image: "assets/images/"
-}, {
-    question: "8. What is not one of the rules for taking care of a Mogwai in 'Gremlins'?",
-    answer: ["Cannot Feed Them Wheat", "No Bright Light", "Never Get Them Wet", "Do Not Feed After Midnight"],
-    correctAnswer: "Cannot Feed Them Wheat",
-    image: "assets/images/"
-}, {
-    question: "9. Which Horror film among these is based on a true story?",
-    answer: ["Get Out", "The Conjuring", "Halloween", "Jeepers Kreepers"],
-    correctAnswer: "Get Out",
-    image: "assets/images/"
-}, {
-    question: "10. True or False: Michael Meyers escaped from a federal penitentiary.",
-    answer: ["True", "False"],
-    correctAnswer: "False. (He escaped from a state hospital.)",
-    image: "assets/images/"
-}];
-
-var game = {
-    questions: questions,
-    currentQuestion: 0,
-    counter: 30,
-    correct: 0,
-    incorrect: 0,
-    countdown: function () {
-        game.counter--;
-        $('#counter'.html(game.counter));
-        if (game.counter <= 0) {
-            console.log("Time's up!");
-            game.timeUp();
-        }
-    },
-    loadQuestion: function () {
-        timer = setInterval(game.countdown, 1000);
-        $('subwrapper').html('<h2>' + question[game.currentQuestion].question + '</h2>');
-        for (var i = 0; i < questions[game.currentQuestion].answer.length; i++) {
-            $('#subwrapper').append('<button class = "answer-button"id= "button-' + i + '"data-name"' + questions[game.currentQuestion.answers[i] + '">' + questions[game.currentQuestion].answers[i] + '</button>']);
-        }
-
-    },
-    nextQuestion: function () {
-        game.counter = 30;
-        $('#counter').html(game.counter);
-        game.currentQuestion++;
-        game.loadQuestion();
-    },
-    timeUp: function () {
-        clearInterval(timer);
-        $('#subwrapper').html('<h2>OUT OF TIME!</h2>');
-        $('#subwrapper').append('<h3>Correct answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
-        if (game.currentQuestion == questions.length - 1) {
-            setTimeout(game, results, 3 * 1000)
-        } else {
-            setTimeout(game.nextQuestion, 3 * 1000);
-        }
-    },
-    results: function () {
-
-    },
-    clicked: function (e) {
-        clearInterval(timer);
-        if ($(e.target).data("name") == question[game.currentQuestion].correctAnswer) {
-            game.answeredCorrectly();
-        } else {
-            game.answeredIncorrectly();
-        }
-    },
-    answeredCorrectly: function () {
-        console.log("YOU GOT IT!");
-        clearInterval(timer);
-        game.correct++;
-        $('#subwrapper').html('<h2>YOU GOT IT RIGHT!</h2>');
-        $('#subwrapper').append('<h3>Correct answer was: ' + questions[game.currentQuestion].correctAnswer + '</h3>');
-        if (game.currentQuestion == questions.length - 1) {
-            setTimeout(game, results, 3 * 1000)
-        } else {
-            setTimeout(game.nextQuestion, 3 * 1000);
-        }
-    },
-
-    answeredIncorrectly: function () {
-        console.log("WRONG!");
-    },
-    reset: function () {
-
-    },
+function nextQuestion() {
+    const isQuestionOver = (quizQuestions.length - 1) === currentQuestion;
+    if (isQuestionOver) {
+        console.log('Game is Over!')
+        displayResult();
+    } else {
+        currentQuestion++;
+        loadQuestion();
+    }
 }
+
+//start a timer of 10 secs to respond to answer chosen
+function timeUp() {
+    clearInterval(timer);
+
+    lost++;
+    preloadImage('lost');
+    setTimeout(nextQuestion, 3 * 1000)
+
+}
+function countDown() {
+    counter--;
+
+    $('#time').html('Timer: ' + counter);
+
+    if (counter === 0) {
+        timeUp();
+    }
+}
+
+/// display ?'s and choices in browswer
+function loadQuestion() {
+    counter = 10;
+    timer = setInterval(countDown, 1000);
+
+    const question = quizQuestions[currentQuestion].question;
+    const choices = quizQuestions[currentQuestion].choices;
+
+    $('#timer').html('Timer: ' + counter);
+    $('#game').html(`
+        <h4>${question}</h4>
+        ${loadChoices(choices)}
+        ${loadRemainingQuestion()}
+   `);
+}
+
+function loadChoices(choices) {
+    let result = '';
+
+    for (let i = 0; i < choices.length; i++) {
+        result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`;
+    }
+
+    return result;
+
+}
+//correct wrong choice selection
+//event delegation
+$(document).on('click', '.choice', function () {
+    clearInterval(timer);
+    const selectedAnswer = $(this).attr('data-answer');
+    const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+
+    if (correctAnswer === selectedAnswer) {
+        score++;
+        console.log(`winssss`);
+        preloadImage('win');
+        setTimeout(nextQuestion, 3 * 1000);
+    } else {
+        lost++;
+        console.log(`lost`);
+        preloadImage('lost');
+        setTimeout(nextQuestion, 3 * 1000);
+    }
+});;
+function displayResult() {
+    const result = `
+        <p>You Got ${score} questions(s) right</p>
+        <p>You Missed ${lost} questions(s)</p>
+        <p>Total Questions ${quizQuestions.length} questions(s) right</p>
+        <button class= "btn btn-primary" id="reset">Reset Game </button>
+    `;
+    $('#game').html(result);
+}
+
+$(document).on('click', '#reset', function () {
+    counter = 10;
+    currentQuestion = 0;
+    score = 0;
+    lost = 0;
+    timer = null;
+
+    loadQuestion();
+});
+function loadRemainingQuestion() {
+    const remainingQuestion = quizQuestions.length - (currentQuestion + 1);
+    const totalQuestion = quizQuestions.length;
+
+    return `Remaining Question: ${remainingQuestion}/${totalQuestion}`;
+}
+function randomImage(images) {
+    const random = Math.floor(Math.floor() * images.length);
+    const randomImage = images[random];
+    return randomImage;
+}
+
+
+//display giphy
+function preloadImage(status) {
+    const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+
+    if (status === 'win') {
+        $('#game').html(`
+            <p class="preload-image"></p>Congratulations,you picked the correct answer!</p>
+            <p class="preload-image"></p>The correct answer is <b>${correctAnswer}</b></p>
+            <img src="${randomImage(funImages)}"/>
+        `);
+    } else {
+        $('#game').html(`
+        <p class="preload-image"></p>The correct answer was <b>${correctAnswer}</b></p >
+        <p class="preload-image"></p>You lost pretty Bad !!</p >
+        <img src="${randomImage(sadImages)}"/>
+        `);
+
+    }
+}
+
+//preloadImage('win');
+loadQuestion();
+//couldn't get my gifs to load properly
+
+
---
+++ assets/javascript/questions.js
@@ -0,0 +1,84 @@
+const quizQuestions = [
+    {
+        question: "1. What dates does the purge fall on in 'The Purge'?",
+        choices: ["December 25- December 26", "Dec. 31st- Jan 1", "March 21 - March 22", "May 13 - May 14"],
+        correctAnswer: "March 21 - March 22",
+        image: "./assets/images/"
+    },
+
+    {
+        question: "2. What is the name of the Villain in 'The Ring'?",
+        choices: ["Camille", "Samara", "Audrey", "Naomi "],
+        correctAnswer: "Samara",
+        image: "assets/images/"
+    },
+    {
+        question: "3. Which movie was based on a video game?",
+        choices: ["Texas Chainsaw Massacre", "Silent Hill", "Child's Play", "Halloween "],
+        correctAnswer: "Silent Hill",
+        image: "assets/images/"
+    },
+
+    {
+        question: "4. In 'Saw', where was Amanda's key hidden?",
+        choices: ["In the intestines of the guy next to her", "In a scorching hot oven", "Behind her eye", "In a safe covered in spikes"],
+        correctAnswer: "In the intestines of the guy next to her",
+        image: "assets/images/"
+    },
+    {
+        question: "5. Which horror movie came out in 1979?",
+        choices: ["Alien", "Black Christmas", "A Nightmare on Elm Street", "Poltergeist"],
+        correctAnswer: "Alien",
+        image: "assets/images/"
+    },
+
+    {
+        question: "6. Which horror film does the quote 'The Power of Christ compels you!' come from?",
+        choices: ["An American Haunting", "Halloween", "A Nightmare on Elm Street", "The Exorcist"],
+        correctAnswer: "The Exorcist",
+        image: "assets/images/"
+    },
+    {
+        question: "7. What is not one of the rules for taking care of a Mogwai in 'Gremlins'?",
+        choices: ["Cannot Feed Them Wheat", "No Bright Light", "Never Get Them Wet", "Do Not Feed After Midnight"],
+        correctAnswer: "Cannot Feed Them Wheat",
+        image: "assets/images/"
+    },
+
+    {
+        question: "8. Which Horror film among these is based on a true story?",
+        choices: ["Get Out", "The Conjuring", "Halloween", "Jeepers Kreepers"],
+        correctAnswer: "Get Out",
+        image: "assets/images/"
+    },
+
+    {
+        question: "9. True or False: Michael Meyers escaped from a federal penitentiary.",
+        choices: ["True","False"],
+        correctAnswer: "False",
+        image: "assets/images/"
+    },
+
+    {
+        question: "10. How many movies are made for the Saw Series?",
+        choices: ["4","8","10","5"],
+        correctAnswer: "8"
+    },
+
+];
+const funImages = [
+    "'/assets/images/creepy smile.gif'",
+    '/assets/images/winner.gif',
+    '/assets/images/happy.gif',
+    '/assets/images/celebrate3.gif',
+    './assets/images/celebrate.gif',
+    './assets/images/celebrate1.gif',
+    './assets/images/celebrate2.gif',
+];
+const sadImages =[
+    './assets/images/scared.gif',
+    './assets/images/lost.gif',
+    './assets/images/failed.gif',
+    './assets/images/ring.gif',
+    './assets/images/creepy smile.gif',
+];
